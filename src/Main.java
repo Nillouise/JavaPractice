f2d1c6bca3c42c3ffecd140a8f294c3d792a4f80
Nillouise <LovelyNihilism@outlook.com>
2016-12-16 20:51:35 +0800

    add blank project to practice

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a []={1,2,3};
+        System.out.println((int)'我');
+        System.out.println("fdsfsd");
+    }
+
+}
