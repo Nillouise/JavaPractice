91a9f520ca8644da042c224e0e1507ddde75746b
Nillouise <LovelyNihilism@outlook.com>
2016-12-18 19:57:01 +0800

    add tcp

---
+++ src/TCPServer.java
@@ -0,0 +1,27 @@
+/**
+ * Created by win7x64 on 2016/12/18.
+ */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class TCPServer {
+    public static void listen()throws Exception
+    {
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        while (true)
+        {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence =inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() +"\n";
+            outToClient.writeBytes(capitalizedSentence);
+
+
+        }
+    }
+
+}
