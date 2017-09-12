784ff98c6f52fe1beac46ae015372614243aa54d
Nillouise <LovelyNihilism@outlook.com>
2017-09-12 16:18:06 +0800

    加入了GC相关的内容

---
+++ src/tmp.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by win7x64 on 2017/9/9.
+ */
+public class tmp
+{
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<>();
+
+        try
+        {
+            list.get(0);
+        } catch (Exception e)
+        {
+//            e.printStackTrace();
+        }
+
+
+        System.out.println("fdsf");
+
+    }
+}
