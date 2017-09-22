c3b1aa64ef473458775b55c5b7a0d664667573d2
Nillouise <LovelyNihilism@outlook.com>
2017-09-22 12:36:39 +0800

    测试了cache池的好处

--- src/myPractice/concurrent/BlockingQueue/SearchFile1.java
+++ src/myPractice/concurrent/BlockingQueue/SearchFile1.java
@@ -1,5 +1,6 @@
 package myPractice.concurrent.BlockingQueue;
 //利用future计算文件夹内所有文件的总行数
+//用cache 池相比每个文件夹都建一条线程，时间由15933 降到 12149
 
 import org.omg.CORBA.INTERNAL;
 
@@ -95,7 +96,31 @@ class ExecutorFileMatch extends FileMatcher
         list.add(task);
         return 0;
     }
+}
 
+//用一个pool而不是看见一个新文件夹就创建新一条线程
+class CachePoolFileMatch extends FileMatcher
+{
+    ExecutorService pool;
+    List<FutureTask<Integer>> list;
+
+    public CachePoolFileMatch(File baseDirectory, ExecutorService pool,List<FutureTask<Integer>> list )
+    {
+        super(baseDirectory);
+        this.pool = pool;
+        this.list = list;
+    }
+
+    @Override
+    public int multithreadProcess(File directory, List<FutureTask<Integer>> noNeed)
+    {
+//        ExecutorFileMatch fileMatcher = new ExecutorFileMatch(directory,pool,this.list);//这里居然忘记改成本类了
+        CachePoolFileMatch fileMatcher = new CachePoolFileMatch(directory,pool,this.list);
+        FutureTask<Integer> task = new FutureTask<Integer>(fileMatcher);
+        pool.submit(task);//感觉是pool不支持多线程插入任务
+        noNeed.add(task);
+        return 0;
+    }
 }
 
 
@@ -103,6 +128,8 @@ public class SearchFile1
 {
     public static void main(String[] args)
     {
+        Long pre = System.currentTimeMillis();
+
         FileMatcher fileMatcher = new FileMatcher(new File("C:\\code"));
         FutureTask<Integer> task = new FutureTask<Integer>(fileMatcher);
         new Thread(task).start();
@@ -116,8 +143,18 @@ public class SearchFile1
         {
             e.printStackTrace();
         }
+        System.out.println(System.currentTimeMillis()-pre);
 
+        pre = System.currentTimeMillis();
+        testCache();
+        System.out.println(System.currentTimeMillis()-pre);
 
+    }
+
+    static void testFix()
+    {
+        //这里我用了fixThreadPool,为了避免等待子文件夹的结果时，父线程还开着（这时可能会把fix爆掉），就无法使用递归，而用了扭曲的写法
+        //应该用 CacheFixedThreadPool，这样可以保持原来的递归写法。
         ExecutorService pool = Executors.newFixedThreadPool(10);
         List<FutureTask<Integer>> list = new ArrayList<>();
 
@@ -146,7 +183,29 @@ public class SearchFile1
             }
         }
         System.out.println(total);
+    }
 
+    static void testCache()
+    {
+        ExecutorService cache = Executors.newCachedThreadPool();
+        List<FutureTask<Integer>> list2 = new ArrayList<>();
+
+        FileMatcher cachePoolFileMatch = new CachePoolFileMatch(new File("C:\\code"),cache,list2);
+
+        FutureTask<Integer> ct = new FutureTask<Integer>(cachePoolFileMatch);
+        list2.add(ct);
+        cache.submit(ct);
+
+        try
+        {
+            System.out.println(ct.get());
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (ExecutionException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
