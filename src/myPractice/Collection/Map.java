ea1538aaabc084b755f72f10b5f2f798bca3b627
Nillouise <lovelynihilism@outlook.com>
2018-01-09 10:17:19 +0800

    加入测试jdbc

---
+++ src/myPractice/Collection/Map.java
@@ -0,0 +1,17 @@
+package myPractice.Collection;
+
+import java.util.*;
+
+public class Map {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("id1",10);
+        map.put("id2",100);
+        for(java.util.Map.Entry<String,Integer> e : map.entrySet())
+        {
+            System.out.printf("%s,%d\n",e.getKey(),e.getValue());
+        }
+    }
+
+}
