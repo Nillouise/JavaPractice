c61a4d2c911d754296576c39956d8b4f9d8c3259
Nillouise <LovelyNihilism@outlook.com>
2018-01-10 14:19:40 +0800

    研究ConcurrentHashMap+加入jdbc Connector

--- src/myPractice/Collection/Map.java
+++ src/myPractice/Collection/Map.java
@@ -6,8 +6,9 @@ public class Map {
 
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
-        map.put("id1",10);
-        map.put("id2",100);
+        map.put(null,10);
+//        map.put("id1",10);
+//        map.put("id2",100);
         for(java.util.Map.Entry<String,Integer> e : map.entrySet())
         {
             System.out.printf("%s,%d\n",e.getKey(),e.getValue());
