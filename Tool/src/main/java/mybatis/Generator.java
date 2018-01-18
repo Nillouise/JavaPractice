bbc4d3dd42efe69d889b4e9af31d359b845894bf
Nillouise <lovelynihilism@outlook.com>
2018-01-18 12:04:27 +0800

    加入DataTimeFormatter

---
+++ Tool/src/main/java/mybatis/Generator.java
@@ -0,0 +1,32 @@
+package mybatis;
+//用maven的插件做出来了，教程参考http://blog.csdn.net/gebitan505/article/details/51788894，调用这java代码，试了一次不行。
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tanjionghong
+ * @version 1.0
+ * @since 2018-01-11
+ **/
+public class Generator {
+    public static void main(String[] args) throws InterruptedException, SQLException, IOException, XMLParserException, InvalidConfigurationException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("D:\\Project\\JavaPractice\\Tool\\src\\main\\resources\\mybatisGeneratorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
